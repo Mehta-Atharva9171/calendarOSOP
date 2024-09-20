96850a4ef08f4f98776c11ee4dea4c773bd96ec2
atharva <atharvashailendramehta@gmail.com>
2024-09-20 18:53:41 +0530

    first functions commit

---
+++ calneder.java
@@ -0,0 +1,26 @@
+
+public class calneder {
+    private int month;
+    private int day;
+    private int choice;
+
+    public void month() {
+
+    }
+
+    public void day() {
+
+    }
+
+    public void days31() {
+
+    }
+
+    public void days30() {
+
+    }
+
+    public void days28() {
+
+    }
+}
