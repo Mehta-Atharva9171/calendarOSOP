7bf28c9264bc0e31c8b9deee8e846f85c1b998bf
atharva <atharvashailendramehta@gmail.com>
2024-09-21 18:31:09 +0530

    day 30 function complete

--- calneder.java
+++ calneder.java
@@ -120,11 +120,11 @@ public class calneder {
 
     public static void days30() {
         dayOfWeek();
-        int day = 30;
+        int day2 = 30;
         switch (days) {
             case 1 -> {
                 System.out.println("Sun Mon Tue Wed Thu Fri Sat");
-                for (int i = 1; i <= day; i++) {
+                for (int i = 1; i <= day2; i++) {
                     System.out.printf("%4d", i);
                     if (i % 7 == 0) {
                         System.out.println();
@@ -132,18 +132,73 @@ public class calneder {
                 }
             }
             case 2 -> {
+                int i = 1;
+                System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+                System.out.printf("%6d", i);
+                for (int j = 2; j <= day2; j++) {
+                    System.out.printf("%4d", j);
+                    if (j == 6 || j == 13 || j == 20 || j == 27) {
+                        System.out.println();
+                    }
+                }
             }
             case 3 -> {
+                int i = 1;
+                System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+                System.out.printf("%10d", i);
+                for (int j = 2; j <= day2; j++) {
+                    System.out.printf("%4d", j);
+                    if (j == 5 || j == 12 || j == 19 || j == 26) {
+                        System.out.println();
+                    }
+                }
             }
             case 4 -> {
+                int i = 1;
+                System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+                System.out.printf("%14d", i);
+                for (int j = 2; j <= day2; j++) {
+                    System.out.printf("%4d", j);
+                    if (j == 4 || j == 11 || j == 18 || j == 25) {
+                        System.out.println();
+                    }
+                }
             }
             case 5 -> {
+                int i = 1;
+                System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+                System.out.printf("%18d", i);
+                for (int j = 2; j <= day2; j++) {
+                    System.out.printf("%4d", j);
+                    if (j == 3 || j == 10 || j == 17 || j == 24) {
+                        System.out.println();
+                    }
+                }
             }
             case 6 -> {
+                int i = 1;
+                System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+                System.out.printf("%22d", i);
+                for (int j = 2; j <= day2; j++) {
+                    System.out.printf("%4d", j);
+                    if (j == 2 || j == 9 || j == 16 || j == 23 || j == 30)
+                        System.out.println();
+                }
             }
             case 7 -> {
+                int i = 1;
+                System.out.println("Sun Mon Tue Wed Thu Fri Sat");
+                System.out.printf("%26d", i);
+                System.out.println();
+                for (int j = 2; j <= day2; j++) {
+                    System.out.printf("%4d", j);
+                    if (j == 8 || j == 15 || j == 22 || j == 29)
+                        System.out.println();
+                }
+
             }
             default -> {
+                System.out.println("Please enter a valid response");
             }
         }
     }
@@ -155,14 +210,16 @@ public class calneder {
     // I have created all the functions for us
     public static void main(String[] args) {
         month();
-        System.out.println();
-        System.out.println("Do You Want to Continue........");
-        System.out.println("1. Y or 2. N");
-        choice = sc.next();
-        if (choice == "Y" || choice == "y") {
-            month();
-        } else {
-            System.exit(0);
-        }
+        /*
+         * System.out.println();
+         * System.out.println("Do You Want to Continue........");
+         * System.out.println("1. Y or 2. N");
+         * choice = sc.next();
+         * if (choice.equals("Y") || choice.equals("y")) {
+         * month();
+         * } else {
+         * System.exit(0);
+         * }
+         */
     }
 }
