4bd9238616467c1a85f2e915d0c7c96601f3dae7
atharva <atharvashailendramehta@gmail.com>
2024-09-23 19:13:15 +0530

    Final todo list commit

--- todoList.java
+++ todoList.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class todoList {
 
     private static int choice;
-    private static ArrayList<String> tasks;
+    private static ArrayList<String> tasks = new ArrayList<>();
 
     private static final Scanner sc = new Scanner(System.in);
 
@@ -16,9 +16,10 @@ public class todoList {
         System.out.println("3. Mark Task Complete");
         System.out.println("4. Delete Task");
         System.out.println("5. Exit");
+        choice();
     }
 
-    public static void task() {
+    public static void todolist() {
         tasks = new ArrayList<>();
     }
 
@@ -27,15 +28,64 @@ public class todoList {
         choice = sc.nextInt();
         switch (choice) {
             case 1 -> {
-                addTask();
+                System.out.print("Enter the task: ");
+                String task = sc.next();
+                todoList.addTask(task);
+            }
+            case 2 -> {
+                todoList.viewTask();
+            }
+            case 3 -> {
+                System.out.println("Enter the task index");
+                int index = sc.nextInt();
+                todoList.markTaskCompleted(index - 1);
+            }
+            case 4 -> {
+                System.out.println("Enter the task index:");
+                int index = sc.nextInt();
+                todoList.deleteTask(index - 1);
+            }
+            case 5 -> {
+                System.out.println("Exiting");
+                System.exit(0);
             }
         }
     }
 
-    public static void addTask() {
-        System.out.println("Enter task : ");
-        String t = sc.nextLine();
-
+    public static void addTask(String task) {
+        tasks.add(task);
+        System.out.println("Task added successfully.");
     }
 
+    public static void viewTask() {
+        System.out.println("Your Task");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+    }
+
+    public static void markTaskCompleted(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.set(index, " Completed " + tasks.get(index));
+            System.out.println("Task marked as complete");
+        } else {
+            System.out.println("Invalid Task Index");
+        }
+    }
+
+    public static void deleteTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+            System.out.println("Task deleted successfully");
+        } else {
+            System.out.println("Invalid Task Index");
+        }
+    }
+
+    public static void main(String[] args) {
+        do {
+            System.out.println("\n");
+            menu();
+        } while (true);
+    }
 }
